9ea7ed9f4ec38bde45943120da8c93ea7980e7d6
Yuriy Lukinov <ylukinov@gsu.edu>
2023-09-13 10:13:15 -0400

    Added CDN link list

--- remove-spaces-and-sp-characters.sh
+++ remove-spaces-and-sp-characters.sh
@@ -1,7 +1,8 @@
+# Remove spaces and special characters from file names for S3 upload
 #!/bin/bash
-find . -name "* *" -type f | rename 's/_/-/g'
-find . -name "*--*" -type f | rename 's/--/-/g'
-find . -name "*---*" -type f | rename 's/---/-/g'
-find . -name "*&*" -type f | rename 's/&/and/g'
-find . -name "*\(*" -type f | rename 's/\(//g'
-find . -name "*\)*" -type f | rename 's/\)//g'
+find ./docs/ -name "* *" -type f | rename 's/ /-/g'
+find ./docs/ -name "*--*" -type f | rename 's/--/-/g'
+find ./docs/ -name "*---*" -type f | rename 's/---/-/g'
+find ./docs/ -name "*&*" -type f | rename 's/&/and/g'
+find ./docs/ -name "*\(*" -type f | rename 's/\(//g'
+find ./docs/ -name "*\)*" -type f | rename 's/\)//g'
